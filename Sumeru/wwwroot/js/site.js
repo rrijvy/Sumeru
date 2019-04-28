8863e4c72b61fe68be7e1228fe3e4d8b088361db
rrijvy <32623510+rrijvy@users.noreply.github.com>
2019-04-28 12:52:33 +0600

    scroll bar add

--- Sumeru/wwwroot/js/site.js
+++ Sumeru/wwwroot/js/site.js
@@ -4,12 +4,15 @@
     function carousel() {
         var i;
         var x = document.getElementsByClassName("mySlides");
+        var y = document.getElementsByClassName("scroll");
         for (i = 0; i < x.length; i++) {
             x[i].style.display = "none";
+            y[i].style.backgroundColor = "#6666663d";
         }
         myIndex++;
-        if (myIndex > x.length) { myIndex = 1 }
+        if (myIndex > x.length) { myIndex = 1; }
         x[myIndex - 1].style.display = "block";
+        y[myIndex - 1].style.backgroundColor = "#599a29";
         setTimeout(carousel, 4000);
     }
 })();
